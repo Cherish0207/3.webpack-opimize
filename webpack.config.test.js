1475bc07b625a56eb0fc67c6055001c6a3abd4dd
Cherish0207 <cheshire_1207@foxmail.com>
2021-08-13 18:52:18 +0800

    3.打包test模块

---
+++ webpack.config.test.js
@@ -0,0 +1,13 @@
+// 单独打包 react 和 react-dom, 开发时引入打包好的文件,这样就不用重新打包了
+const path = require("path");
+module.exports = {
+  entry: {
+    test: "./src/test.js",
+  },
+  mode: "development",
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "dist")
+  },
+  plugins: [],
+};
