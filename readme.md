a4745fe1951b1b1e8da6f1147e235c194d28c89f
Cherish0207 <cheshire_1207@foxmail.com>
2021-08-13 15:07:22 +0800

    初始化项目

---
+++ readme.md
@@ -0,0 +1,5 @@
+## webpack 优化手段
+
+### 初始化项目
+
+`npm i webpack webpack-cli html-webpack-plugin @babel/core babel-loader @babel/preset-env @babel/preset-react -D`
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+module.exports = {
+  entry: "./src/index.js",
+  mode: "development",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [new HtmlWebpackPlugin()],
+};
